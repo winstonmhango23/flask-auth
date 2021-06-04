e3f7330a16caf0e22111e8fd3b3bf2d33a780c2a
winstonmhango23 <winstonmhango23@gmail.com>
2021-06-04 16:22:01 +0200

    Create database.sql
    Created a script for creating database table

---
+++ part 1/database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+   PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES
+(1, 'test', 'test', 'myemail@gmail.com');
+
